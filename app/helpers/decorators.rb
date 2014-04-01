00762a8c399e31f5ddb00d1ee2c2a894fcd6dd40
Alex Kukushkin <alex@kukushk.in>
2014-04-01 16:04:26 +0300

    Restyled page preview

--- app/helpers/decorators.rb
+++ app/helpers/decorators.rb
@@ -1,3 +1,6 @@
+# Returns publication state as icon,
+# unless it is :published, in which case nothing is rendered.
+#
 def admin_pages_publication_state_as_icon( page, lang )
   page_content = page.content( lang )
   state = page_content.try(:publication_state) || :not_published
@@ -9,21 +12,28 @@ def admin_pages_publication_state_as_icon( page, lang )
   end
 end
 
+# Returns page title for given +lang+, with respect to publication state.
+#
 def admin_pages_title_as_text( page, lang )
   page_content = page.content( lang )
   return "<span class='nodata'>no translation</span>" if page_content.blank?
   case page_content.publication_state.to_sym
-    when :published then page_content.title
+    when :published then h page_content.title
     when :hidden then "<span class='state-hidden'>#{h page_content.title}</span>"
     else
       "<span class='state-not-published'>#{h page_content.title}</span>"
   end
 end
 
+# Returns title and publication state as icon and text.
+#
 def admin_pages_title_as_icon_and_text( page, lang )
   admin_pages_publication_state_as_icon( page, lang )+" "+admin_pages_title_as_text( page, lang )
 end
 
+
+# Returns link to a page for given +lang+, with respect to publication state.
+#
 def admin_pages_link_as_text( page, lang )
   page_content = page.content( lang )
   return "<span class='nodata'>no content</span>" if page_content.blank?
@@ -35,10 +45,15 @@ def admin_pages_link_as_text( page, lang )
   end
 end
 
+# Returns link and publication state as icon and text.
+#
 def admin_pages_link_as_icon_and_text( page, lang )
   admin_pages_publication_state_as_icon( page, lang )+" "+admin_pages_link_as_text( page, lang )
 end
 
+# Returns full path to current page in given +lang+,
+# links are generated as icon and text
+#
 def admin_pages_url_as_text( page, lang )
   page.ancestors_and_self.sort_by(&:depth).map do |p|
     if p.root?
@@ -48,3 +63,4 @@ def admin_pages_url_as_text( page, lang )
     end
   end.join(" / ")
 end
+
